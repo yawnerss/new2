08a871bc7bc4545cd33b743bc08d0c0ced04987e
yawnerss <haburat9@gmail.com>
2026-01-24 12:20:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const adminHTML = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stress Test Control Panel</title>
+    <title>R1C4RD0 CTRL PANEL | BTR</title>
     <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
         body {
@@ -1293,4 +1293,5 @@ server.listen(PORT, '0.0.0.0', () => {
 ╚════════════════════════════════════════╝
   `);
 });
+
 
